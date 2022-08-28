53f9e06d49de6e8dd415a19b6c2dc6119470634b
Tkai87 <tkai@usc.edu>
2022-08-27 23:22:08 -0700

    DB connected

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
+var config = require("./config");
 
 app.use(express.static("public"));
+
+mongoose.connect(`mongodb+srv://TK:${config.mongoPW}@cluster0.szjqwe3.mongodb.net/threads?retryWrites=true&w=majority`)
+.then(() => console.log("DB connected!!"))
+.catch((err) => console.log(err));
 
 app.listen(PORT, () => {
     console.log("server is running!!")
